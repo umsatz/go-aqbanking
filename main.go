7dfed64542f108dfab9c575e48059de39e31e38a
Raphael Randschau <nicolai86@me.com>
2014-06-02 12:18:28 +0200

    executes without stacktrace

--- main.go
+++ main.go
@@ -8,17 +8,30 @@ import (
 
 /*
 #cgo LDFLAGS: -laqbanking
+#cgo LDFLAGS: -lgwenhywfar
 #cgo CFLAGS: -I/usr/local/include/gwenhywfar4
 #cgo CFLAGS: -I/usr/local/include/aqbanking5
 #include <aqbanking5/aqbanking/banking.h>
 */
 import "C"
 
+// type AQBanking struct {
+// 	Name string
+// 	Ptr  *C.AB_BANKING
+// }
+
+// func NewAQBanking(name string) *AQBanking {
+// 	inst := &AQBanking{name, nil}
+// }
+
 func main() {
 	var ab *C.AB_BANKING
 	appName := C.CString("golib")
 	defer C.free(unsafe.Pointer(appName))
 
+	//
+	// SETUP
+	//
 	ab = C.AB_Banking_new(appName, nil, 0)
 	if err := C.AB_Banking_Init(ab); err != 0 {
 		log.Fatal("unable to initialize aqbanking: %d\n", err)
@@ -27,10 +40,16 @@ func main() {
 		log.Fatal("unable to initialize aqbanking online: %d\n", err)
 	}
 
+	var gui *C.GWEN_GUI
+	gui = C.GWEN_Gui_new()
+	C.GWEN_Gui_SetGui(gui)
+
+	// list version, debug stuff
 	var major, minor, patchlevel, build C.int
 	C.AB_Banking_GetVersion(&major, &minor, &patchlevel, &build)
 	fmt.Printf("using aqbanking %d.%d.%d\n", major, minor, patchlevel)
 
+	// list known accounts
 	var account_list *C.AB_ACCOUNT_LIST2
 	account_list = C.AB_Banking_GetAccounts(ab)
 	if account_list == nil {
@@ -58,8 +77,10 @@ func main() {
 	C.AB_Account_free(a)
 	C.AB_Account_List2_FreeAll(account_list)
 
-	// TODO AB_JobGetBalance_new
-
+	//
+	// TEARDOWN
+	//
+	// fmt.Printf("%d\n", ab.onlineInitCount)
 	if err := C.AB_Banking_OnlineFini(ab); err != 0 {
 		log.Fatal("unable to free aqbanking online: %d\n", err)
 	}
