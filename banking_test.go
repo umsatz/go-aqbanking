852588ef897f93f45b71e5f3c979071b3adc4ecc
Raphael Randschau <nicolai86@me.com>
2014-06-05 19:45:37 +0200

    add simple banking test

---
+++ banking_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestDefaultAQBankingInstance(t *testing.T) {
+	aq, err := DefaultAQBanking()
+	if err != nil {
+		t.Fatalf("unable to create aqbanking instance")
+	}
+	aq.Free()
+}
+
+func TestNewAQBankingInstance(t *testing.T) {
+	defer os.RemoveAll("./tmp")
+
+	aq, err := NewAQBanking("example", "./tmp")
+
+	if err != nil {
+		t.Fatalf("unable to create custom aqbanking instance")
+	}
+	aq.Free()
+}
