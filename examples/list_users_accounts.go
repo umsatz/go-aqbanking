ddc6673ad5215270a242f14377fd252beb824246
Raphael Randschau <nicolai86@me.com>
2014-07-13 17:29:03 +0200

    add example to list transactions

--- examples/list_users_accounts.go
+++ examples/list_users_accounts.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"log"
 
-	aq "github.com/umsatz/go-aqbanking"
+	aqb "github.com/umsatz/go-aqbanking"
 )
 
 func main() {
-	aq, err := aq.DefaultAQBanking()
+	aq, err := aqb.DefaultAQBanking()
 	// alternativly, customize the aqbanking path:
 	// aq, err := aq.NewAQBanking("custom", "./tmp")
 	if err != nil {
@@ -22,12 +22,20 @@ func main() {
 		aq.Version.Patchlevel,
 	)
 
+	for _, pin := range aqb.LoadPins("pins.json") {
+		aq.RegisterPin(&pin)
+	}
+
 	userCollection, err := aq.Users()
 	if err != nil {
 		log.Fatal("unable to list users: %v", err)
 	}
 	defer userCollection.Free()
 	fmt.Printf("found %d users.\n", len(userCollection.Users))
+
+	for _, user := range userCollection.Users {
+		fmt.Printf("User %d\n", user.Id)
+	}
 
 	accountCollection, err := aq.Accounts()
 	if err != nil {
@@ -36,4 +44,11 @@ func main() {
 	defer accountCollection.Free()
 
 	fmt.Printf("found %d accounts.\n", len(accountCollection.Accounts))
+
+	for _, account := range accountCollection.Accounts {
+		fmt.Printf("Account %v\n", account.Name)
+
+		transactions, _ := aq.Transactions(&account, nil, nil)
+		fmt.Printf("%d transactions\n", len(transactions))
+	}
 }
