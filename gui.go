90b7b434c389654a56a1fd90280c526eab5f4c7c
Raphael Randschau <nicolai86@me.com>
2014-10-19 15:08:03 +0200

    add comments to the gui

--- gui.go
+++ gui.go
@@ -17,7 +17,7 @@ type gui struct {
 }
 
 func newGui(interactive bool) *gui {
-	var abGui *C.struct_GWEN_GUI = C.GWEN_Gui_CGui_new()
+	abGui := C.GWEN_Gui_CGui_new()
 
 	if !interactive {
 		C.GWEN_Gui_SetFlags(abGui, C.GWEN_GUI_FLAGS_ACCEPTVALIDCERTS|C.GWEN_GUI_FLAGS_NONINTERACTIVE)
@@ -30,7 +30,7 @@ func newGui(interactive bool) *gui {
 	// C.GWEN_Logger_SetLevel(C.CString(C.AQBANKING_LOGDOMAIN), C.GWEN_LoggerLevel_Error)
 	// C.GWEN_Logger_SetLevel(C.CString(C.GWEN_LOGDOMAIN), C.GWEN_LoggerLevel_Error)
 
-	var dbPins *C.GWEN_DB_NODE = C.GWEN_DB_Group_new(C.CString("pins"))
+	dbPins := C.GWEN_DB_Group_new(C.CString("pins"))
 	C.GWEN_Gui_CGui_SetPasswordDb(abGui, dbPins, 1)
 
 	return &gui{
@@ -47,6 +47,9 @@ func (g *gui) attach(aq *AQBanking) {
 	C.AB_Gui_Extend(g.ptr, aq.ptr)
 }
 
+// RegisterPin registers a given Pin code with the aqbanking gui.
+// required to allow go-aqbanking to operate non-interactively
+// Note that the pins must be registered prior to requesting accounts or transactions
 func (ab *AQBanking) RegisterPin(pin Pin) {
 	str := fmt.Sprintf("PIN_%v_%v=%v\n", pin.BankCode(), pin.UserId(), pin.Pin())
 	pinLen := len(str)
